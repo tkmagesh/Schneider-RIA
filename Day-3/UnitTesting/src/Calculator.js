602a0586163114482b242a7f59dcbf62a33171e7
tkmagesh <tkmagesh77@gmail.com>
2013-09-19 10:03:49 -0700

    Day-4 Revision

--- Day-3/UnitTesting/src/Calculator.js
+++ Day-3/UnitTesting/src/Calculator.js
@@ -7,7 +7,6 @@ function Calculator(){
 	      return isNaN(x) ?  0 : parseInt(x);
 	    if (typeof x == "function") return parseArg(x());
 	    if (typeof x.length != "undefined") return that.add.apply(that,x);
-
 	    return 0;
 	  }
 	  return (arguments.length == 1) ? parseArg(arguments[0]) : parseArg(arguments[0]) + this.add.apply(this,[].splice.call(arguments,1));
